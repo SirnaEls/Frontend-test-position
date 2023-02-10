dff391b0c4cd46c81a3af6c493984ba158408ec8
Nassir El abbassi <nassirelabbassi@mbp-de-nassir.home>
2023-02-10 16:39:11 +0100

    front finish

--- src/components/Assessments/ScssAssessment/index.js
+++ src/components/Assessments/ScssAssessment/index.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from "react";
 
 export default function ScssAssessment() {
   return (
@@ -10,11 +10,30 @@ export default function ScssAssessment() {
         In this first test we will try out your skills in styling elements.
       </p>
       <p className="subpixel-antialiased w-fit m-2">
-        Your goal is to reproduce the checkerboard presented in the picture below with HTML elements, and
-        SCSS or Tailwind styling. <span className="font-bold">Before starting your work, please refer
-        to the README for more detailed directions!</span>
+        Your goal is to reproduce the checkerboard presented in the picture
+        below with HTML elements, and SCSS or Tailwind styling.{" "}
+        <span className="font-bold">
+          Before starting your work, please refer to the README for more
+          detailed directions!
+        </span>
       </p>
       <img src="/images/styling-test.png" alt="styling-test" />
+      <div className="box">
+        <div className="top-box">
+          <div className="top-box1">john</div>
+          <div className="top-box2">
+            <p>doe</p>
+          </div>
+          <div className="top-box3">john</div>
+        </div>
+        <div className="bottom-box">
+          <div className="bottom-1">doe</div>
+          <div className="bottom-right">
+            <div className="bottom-2">doe</div>
+            <div className="bottom-3">john</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
