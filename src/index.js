5bcebd28c767dfc4fdef210c957cbc0040d93b5b
Nassir El abbassi <nassirelabbassi@mbp-de-nassir.home>
2023-02-10 16:58:59 +0100

    last commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  gql,
+} from "@apollo/client";
+import App from "./App";
 
 const client = new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
-    cache: new InMemoryCache(),
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
 });
 
+// // const query = gql`
+// //   query GetCakes {
+// //     cakes {
+// //       description
+// //       id
+// //       image
+// //       title
+// //     }
+// //   }
+// `;
+
+client
+  .query({
+    query: gql`
+      query GetCakes {
+        cakes {
+          description
+          id
+          image
+          title
+        }
+      }
+    `,
+  })
+  .then((result) => console.log("Nassir", result));
+
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <App/>
-    </ApolloProvider>,
-    document.getElementById('root')
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
 );
